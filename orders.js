46b450298a50e53b10763a7359dd03cb130ea676
SAHIL BODKHE <sahilbodkhe45@gmail.com>
2023-03-11 16:57:51 +0530

    dashboard

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'Air conditioner',
+    productNumber: '85361',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+  {
+    productName: 'Car',
+    productNumber: '85391',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+  },
+  {
+    productName: 'Ruko F11 PRO drone',
+    productNumber: '85381',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+  {
+    productName: 'IPHONE 14',
+    productNumber: '85000',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+  {
+    productName: 'Sofa',
+    productNumber: '82110',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+];
