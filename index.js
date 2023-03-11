bea23e0202e420c2d51a4712ace005cb20426c96
Skyhero-alt <skyhero.code@gmail.com>
2023-03-11 21:22:39 +0530

    fix: Modified the current page for Coupons

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-const themeToggler = document.querySelector('.theme-toggler');
-menuBtn.addEventListener('click', () => {
-  sideMenu.style.display = 'block';
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
 });
-closeBtn.addEventListener('click', () => {
-  sideMenu.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
 });
-themeToggler.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme-variables');
-  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
 Orders.forEach((order) => {
-  const tr = document.createElement('tr');
+  const tr = document.createElement("tr");
   const trContent = `<tr>
 <td>${order.productName}</td>
 <td>${order.productNumber}</td>
 <td>${order.paymentStatus}</td>
+<td>${order.paymentStatus}</td>
+<td>${order.paymentStatus}</td>
+
 <td class="${
-    order.shipping === 'Declined'
-      ? 'danger'
-      : order.shipping === 'pending'
-      ? 'warning'
-      : 'primary'
+    order.shipping === "Declined"
+      ? "danger"
+      : order.shipping === "pending"
+      ? "warning"
+      : "primary"
   }">${order.shipping}</td>
 <td class="primary">Details</td>
+<td><button class="danger modelbutt" onclick="openModal()">Edit coupon</button></td>
 </tr>`;
   tr.innerHTML = trContent;
-  document.querySelector('table tbody').appendChild(tr);
+  document.querySelector("table tbody").appendChild(tr);
 });
+
+//  Modal popup js
+
+var modal = document.getElementById("myModal");
+
+// Open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Close the modal when the user clicks outside of it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// The add coupon model -------------------------------------------------------------------
+var cmodal = document.getElementById("addCouponModal");
+
+// Open the modal
+function openCModal() {
+  cmodal.style.display = "block";
+}
+
+// Close the modal
+function closeCModal() {
+  cmodal.style.display = "none";
+}
+
+// Close the modal when the user clicks outside of it
+window.onclick = function (event) {
+  if (event.target == cmodal) {
+    cmodal.style.display = "none";
+  }
+};
